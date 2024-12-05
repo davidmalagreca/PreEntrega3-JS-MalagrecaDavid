8db0fc6c1b2f8aac289f3f7e0dc4da88acf9c366
David Malagreca <malagrecadavid@gmail.com>
2024-12-05 17:22:12 -0300

    añadi carrito

--- index2.js
+++ index2.js
@@ -12,59 +12,42 @@ document.addEventListener("DOMContentLoaded", function() {
             alert("No se encontró lo buscado.");
         }
     });
-});
 
-let cantidadRemeras = 0;
-
-let precioAcumulado = 0;
-
-const precioRemera = 23500;
-
-
-document.addEventListener("DOMContentLoaded", function() {
+    let cantidadRemeras = 0;
+    let precioAcumulado = 0;
+    const precioRemera = 23500;
 
     const botonAñadir = document.querySelector("#pArticulo .botonAñadir button");
-
     const botonDescuento = document.createElement("button");
-
     botonDescuento.textContent = "Descontar -";
-
     document.querySelector("#pArticulo .botonAñadir").appendChild(botonDescuento);
 
+    function actualizarCarrito() {
+        const carritoEmoji = document.getElementById("carritoEmoji");
+        if (cantidadRemeras > 0) {
+            carritoEmoji.textContent = `🛒 (${cantidadRemeras})`;
+        } else {
+            carritoEmoji.textContent = `🛒 (-)`;
+        }
+    }
 
     botonAñadir.addEventListener("click", function() {
-
         cantidadRemeras++;
-
         precioAcumulado += precioRemera;
-
         console.log(`Cantidad de remeras: ${cantidadRemeras}`);
-
         console.log(`Precio acumulado: $${precioAcumulado}`);
-
+        actualizarCarrito(); 
     });
-
-
 
     botonDescuento.addEventListener("click", function() {
-
         if (cantidadRemeras > 0) {
-
             cantidadRemeras--;
-
             precioAcumulado -= precioRemera;
-
             console.log(`Cantidad de prendas: ${cantidadRemeras}`);
-
             console.log(`Precio total: $${precioAcumulado}`);
-
+            actualizarCarrito(); 
         } else {
-
             console.log("No hay prendas para descuento");
-
         }
-
     });
-
 });
-
